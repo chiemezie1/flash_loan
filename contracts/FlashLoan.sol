eb0e0c5ba2b19b36434b991d989bc5f4953b2efe
Absolute <106808580+chiemezie1@users.noreply.github.com>
2023-06-05 19:21:30 +0100

    flashloan loan contract

--- contracts/FlashLoan.sol
+++ contracts/FlashLoan.sol
@@ -1,34 +1,63 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.10;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import {FlashLoanSimpleReceiverBase} from "https://github.com/aave/aave-v3-core/...";
+import {IPoolAddressesProvider} from "https://github.com/aave/aave-v3-core/...";
+import {IERC20} from "https://github.com/aave/aave-v3-core/...";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+contract FlashLoan is FlashLoanSimpleReceiverBase {
+    address payable owner;
+    constructor (address _addressProvider) FlashLoanSimpleReceiverBabe(IPoolAddressesProvider (_addressProvider)) {
+        owerner = payable(msg.sender);
+    }
+    
 
-    event Withdrawal(uint amount, uint when);
+     // paying back the loan with the ERC20 approve function
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
+     function executeOperation(
+    address asset,
+    uint256 amount,
+    uint256 premium,
+    address initiator,
+    bytes calldata params
+  ) external override returns (bool){
+
+    uint amountOwened = (amount + premium);
+    IERC20(asset).approve(address(POOL), amountOwened);  
+    return true;
+  }
+
+    // this is the function that requestFlashLoad
+
+  function requestFlashLoad(address _token, uint256 _amount) public {
+    address receiverAddress = address(this);
+    address asset = _token;
+    uint256 amount = _amount;
+    bytes memory params ="";
+    uint16 referralCode = 0;
+
+    POOL.FlashLoanSimple(
+        receiverAddress,
+        asset,
+        amount,
+        params,
+        referralCode
         );
+  }
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
-    }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+  function getBalanceOf(address _tokenAddress) external view returns(uint265){
+    return IERC20(_tokenAddress).balanceOf(address(this));
+  }
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+  function withdrawToken(address _tokenAddress) external onlyOwner {
+    address token = IERC20(_tokenAddress);
+    token.transfer(msg.sender, token.balanceOf(address(this)));
+  }
 
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
-    }
+  modifier onlyOwner{
+    require(msg.sender == owner, 'Only owner can withdraw token');
+    _;
+  }
+  receive () external payable {}
 }
